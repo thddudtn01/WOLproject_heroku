1b66c5cdc4d5b4cfae3069247807e125cb52a31c
thddudtn01 <54704254+thddudtn01@users.noreply.github.com>
2021-01-07 17:37:56 +0900

    firest upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT
 app.get("/", (req, res) => {
 res.send({ hello: "world" });
 });
-app.listen(3000);
+app.listen(PORT);
