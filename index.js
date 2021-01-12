8e84d44bf1b0b68add9786e1d3395548eefc0526
thddudtn01 <54704254+thddudtn01@users.noreply.github.com>
2021-01-12 14:28:31 +0900

    a

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT
+var com = 0;
 app.get("/", (req, res) => {
+res.send({ WOLproject: x });
+});
+app.get("/1/", (req, res) => {
 res.send({ WOLproject: "good" });
+com = 1;
 });
 app.listen(PORT);
